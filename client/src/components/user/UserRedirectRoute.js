d1add18408bfc5dc56a27cd7ed9287ce11cf9a3a
Hin <tonghohin77@gmail.com>
2023-01-26 17:30:58 -0400

    Chore

--- client/src/components/user/UserRedirectRoute.js
+++ client/src/components/user/UserRedirectRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-// Redirect authenticated users to main portal when they try to visit login/riegister page
+// Redirect authenticated users to main portal when they try to visit login/register page
 function UserRedirectRoute(props) {
   const userReducer = useSelector((store) => store.user);
 
